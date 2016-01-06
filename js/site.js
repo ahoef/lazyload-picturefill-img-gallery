11588eae6b38d4c98557f12601aa70cfefe389bd
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2016-01-05 22:08:54 -0500

    add jsdocs & clean up typos and indentation

--- js/site.js
+++ js/site.js
@@ -1,29 +1,31 @@
-jQuery(function($) {
-    $(window).load(function() {
+$(document).ready(function() {
 
-    	var isMobile = screen.width <= 640,
-	    	isRetina = window.devicePixelRatio > 1;
-	    	$imgLazyload = $('.img-lazyload');
+	var isMobile = screen.width <= 640,
+    	isRetina = window.devicePixelRatio > 1,
+    	$imgLazyload = $('.img-lazyload');
 
-        $imgLazyload.each(function() {
+  /**
+  * Attach each image's appropriate data-src attribute 
+  * according to screen size
+  */
+  $imgLazyload.each(function() {
+  	var $this = $(this),
+  		desktopImgSrc = $this.attr('data-desktop'),
+  		mobileImgSrc = $this.attr('data-mobile'),
+  		retinaImgSrc = $this.attr('data-retina'),
+  		dataSrc = isMobile ? mobileImgSrc : (isRetina ? retinaImgSrc : desktopImgSrc);
 
-	      	var $this = $(this);
-	      		desktopImgSrc = $this.attr('data-desktop'),
-	      		mobileImgSrc = $this.attr('data-mobile'),
-	      		retinaImgSrc = $this.attr('data-retina'),
-	      		dataSrc = isMobile ? mobileImgSrc : 
-	      					(isRetina ? retinaImgSrc :
-	      						desktopImgSrc);
+    $this.attr('data-src', dataSrc);
+  });
 
-	      	$this.attr('data-src', dataSrc);
-
-        });
-
-        $imgLazyload.unveil(200, function() {
-		  $(this).load(function() {
-		    this.style.opacity = 1;
-		  });
-		});
+  /**
+  * Set unveil to update each image's data-src attr to its 
+  * src attr, 300px before on screen
+  */
+  $imgLazyload.unveil(300, function() {
+	  $(this).load(function() {
+	    this.style.opacity = 1;
+	  });
+	});
 		
-    });
 });
